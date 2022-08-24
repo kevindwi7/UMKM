327d300454e1c25b5086c5fb177d06dc767930d3
Trevincen <ttambunan@student.ciputra.ac.id>
2022-08-24 13:13:37 +0700

    ganti ganti buat merge ke main

--- UMKM/View/TabBarView/HomeView.swift
+++ UMKM/View/TabBarView/HomeView.swift
@@ -9,12 +9,30 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            ScrollView{
+                ForEach(0...10, id: \.self){ project in
+                    ProjectCardView()
+                    
+                }.padding()
+            }
+            .navigationTitle("Proyek")
+                .frame(width: UIScreen.main.bounds.width)
+                .background(Color.primaryGray)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    Button {
+                        print(123)
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                }
+        }
     }
 }
 
-//struct HomeView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        HomeView()
-//    }
-//}
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
