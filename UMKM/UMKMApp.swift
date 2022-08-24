14e66b0c331f133eacfa5aaa275b191760b413d0
Kevin Dwi <dwikevin07@gmail.com>
2022-08-24 10:13:44 +0700

    Initial Commit

---
+++ UMKM/UMKMApp.swift
@@ -0,0 +1,17 @@
+//
+//  UMKMApp.swift
+//  UMKM
+//
+//  Created by Kevin  Dwi on 24/08/22.
+//
+
+import SwiftUI
+
+@main
+struct UMKMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
