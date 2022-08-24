03352ce240a7a20647b57974eb012470bc1d3d31
Trevincen <ttambunan@student.ciputra.ac.id>
2022-08-24 14:27:11 +0700

    create detailprojectview

--- UMKM/View/TabBarView/ProjectCardView.swift
+++ UMKM/View/TabBarView/ProjectCardView.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 
 struct ProjectCardView: View {
+    
+    @Binding var isShowingDetailProjectView:Bool
     var body: some View {
         VStack(alignment:.leading,spacing: 8){
             Text("Bersih bersih basecamp").font(.system(size: 15, weight: .bold, design: .default))
             HStack{
-                Image("location").resizable().frame(width: 15, height: 18).scaledToFit()
+                Image("location").resizable().scaledToFit().frame(width: 20, height: 20)
                 Text("Basecamp").font(.system(size: 12, weight: .medium, design: .default))
                 Spacer()
                 Image(systemName: "chevron.right")
             }
             HStack{
-                Image(systemName: "calendar")
+                Image(systemName: "calendar").resizable().scaledToFit().frame(width: 20, height: 20)
                 Text("21 Aug 22")
                 Text("|")
                 Text("8.00 - 10.00 AM")
@@ -26,11 +28,15 @@ struct ProjectCardView: View {
         }.padding()
             .frame(width: UIScreen.main.bounds.width/1.2, alignment:.leading)
         .background(.white)
-        .cornerRadius(15)    }
+        .cornerRadius(16)
+        .onTapGesture {
+            isShowingDetailProjectView.toggle()
+        }
+    }
 }
 
 struct ProjectCardView_Previews: PreviewProvider {
     static var previews: some View {
-        ProjectCardView()
+        ProjectCardView(isShowingDetailProjectView: .constant(false))
     }
 }
