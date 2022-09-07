d145738b380a618785fdaf3d7ef4737b934b99d7
Trevincen <ttambunan@student.ciputra.ac.id>
2022-09-07 10:29:47 +0700

    buat view onboarding

--- UMKM/UMKMApp.swift
+++ UMKM/UMKMApp.swift
@@ -16,7 +16,8 @@ struct UMKMApp: App {
     var body: some Scene {
         WindowGroup {
 //            LoginView(vm: MainViewModel(container: CKContainer.default()))
-            TabBarView()
+//            TabBarView()
+            OnboardingView()
         }
     }
 }
---
+++ UMKM/View/Onboarding/OnboardingView.swift
@@ -0,0 +1,75 @@
+//
+//  OnboardingView.swift
+//  UMKM
+//
+//  Created by Trevincen Tambunan on 07/09/22.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    
+    @State var komunitas = ""
+    @State var divisi = ""
+    @State var pengalaman = ""
+    var placeholder = "Divisi"
+    var dropDownList = ["PSO", "PFA", "AIR", "HOT"]
+    
+    var body: some View {
+        VStack(spacing:10) {
+            HStack {
+                Text("Halo")
+                Text("Nama,").foregroundColor(.yellow)
+            }.font(.system(size: 34, weight: .bold, design: .default))
+            Text("Cerita dikit yuk!").font(.system(size: 34, weight: .regular, design: .default))
+            //            Spacer()
+            HStack{
+                Image(systemName: "house").foregroundColor(.blue)
+                TextField("Komunitas", text: $komunitas)
+            }
+            Divider()
+            HStack{
+                Image(systemName: "person.2").foregroundColor(.blue)
+                Menu {
+                    ForEach(dropDownList, id: \.self){ divisi in
+                        Button(divisi) {
+                            self.divisi = divisi
+                        }
+                    }
+                } label: {
+                    VStack(spacing: 5){
+                        HStack{
+                            Text(divisi.isEmpty ? placeholder : divisi)
+                                .foregroundColor(.black)
+//                                .foregroundColor(divisi.isEmpty ? .gray : .black)
+                                .opacity(divisi.isEmpty ? 0.3 : 1)
+                            Spacer()
+                        }
+                    }
+                }
+            }
+            
+            Divider()
+            HStack(alignment:.top){
+                Image(systemName: "person.fill.questionmark").foregroundColor(.blue).padding(.vertical,3)
+                Spacer()
+                VStack(alignment:.leading) {
+                    Text("Tentang Kamu").font(.system(size: 18, weight: .regular, design: .rounded)).opacity(0.3)
+                    Text("Ceritakan pengalaman atau keahlian kamu, sampaikan juga hal-hal yang menurut kamu menarik atau penting").font(.system(size: 14, weight: .regular, design: .rounded)).opacity(0.3)
+                    ZStack {
+                        TextEditor(text: $pengalaman)
+                        Text(pengalaman).opacity(0).padding(8)
+                    }
+                }
+            }
+            Divider()
+            Spacer()
+        }.padding()
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
