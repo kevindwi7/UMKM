8f23f9c17eaa8013eb046cb976ac287d92cf5a92
Trevincen <ttambunan@student.ciputra.ac.id>
2022-09-09 23:06:15 +0800

    pengecekan daftar anggota apakah kosong atau tidak

--- UMKM/View/DetailProjectView/DetailTask/AnggotaSheetView.swift
+++ UMKM/View/DetailProjectView/DetailTask/AnggotaSheetView.swift
@@ -11,6 +11,7 @@ struct AnggotaSheetView: View {
     
     @StateObject var vm:HomeViewModel
     @Environment(\.dismiss) var dismiss
+    @State var test:Int = 0
     
     init(vm: HomeViewModel) {
         _vm = StateObject(wrappedValue: vm)
@@ -18,40 +19,64 @@ struct AnggotaSheetView: View {
     
     var body: some View {
         NavigationView {
-            VStack(alignment:.leading,spacing: 10) {
-                Text("Pilih anggota untuk tugas").font(.system(size: 14, weight: .regular, design: .default)).padding(.horizontal)
-                Text("Pel Lantai").font(.system(size: 14, weight: .regular, design: .default)).padding(.horizontal)
-                List(0..<30) {_ in
-                    VStack {
-                        HStack {
-                            Text("ASd").font(.system(size: 14, weight: .medium, design: .rounded))
-                            Spacer()
-                            Image(systemName: "person.fill.checkmark").opacity(0.5)
-                            Image(systemName: "person.fill.xmark").opacity(0.5)
+            if(test == 0){
+                EmptyListView()
+                    .frame(width: UIScreen.main.bounds.width,height: UIScreen.main.bounds.height)
+                    .background(SwiftUI.Color.primaryGray)
+                    .padding()
+                    .navigationTitle("Anggota")
+                    .toolbar(content: {
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button {
+                                dismiss()
+                                HapticManager.instance.notification(type: .warning)
+                            } label: {
+                                Text("Cancel").accessibilityLabel("Kembali")
+                            }
+                            
+                        }
+
+                    }
+                    )
+                    .navigationBarTitleDisplayMode(.inline)
+            }else{
+                VStack(alignment:.leading,spacing: 10) {
+                    Text("Pilih anggota untuk tugas").font(.system(size: 14, weight: .regular, design: .default)).padding(.horizontal).padding(.top)
+                    Text("Pel Lantai").font(.system(size: 14, weight: .regular, design: .default)).padding(.horizontal)
+                    List(0..<test) {_ in
+                        VStack {
+                            HStack {
+                                Text("ASd").font(.system(size: 14, weight: .medium, design: .rounded))
+                                Spacer()
+                                Image(systemName: "person.fill.checkmark").opacity(0.5)
+                                Image(systemName: "person.fill.xmark").opacity(0.5)
+                            }
                         }
                     }
-                }
-                .cornerRadius(10)
-                .padding()
-                .listStyle(.plain)
-            }
-            .frame(width: UIScreen.main.bounds.width)
-            .background(SwiftUI.Color.primaryGray)
-            .navigationTitle("Anggota")
-            .toolbar(content: {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button {
-                        dismiss()
-                        HapticManager.instance.notification(type: .warning)
-                    } label: {
-                        Text("Cancel").accessibilityLabel("Kembali")
-                    }
                     
+                    .cornerRadius(10)
+                    .listStyle(.plain)
+                    .padding()
                 }
+                .frame(width: UIScreen.main.bounds.width)
+                .background(SwiftUI.Color.primaryGray)
+                .navigationTitle("Anggota")
+                .toolbar(content: {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button {
+                            dismiss()
+                            HapticManager.instance.notification(type: .warning)
+                        } label: {
+                            Text("Cancel").accessibilityLabel("Kembali")
+                        }
+                        
+                    }
 
+                }
+                )
+                .navigationBarTitleDisplayMode(.inline)
             }
-            )
-            .navigationBarTitleDisplayMode(.inline)
+                        
             
         }
         .onAppear{
